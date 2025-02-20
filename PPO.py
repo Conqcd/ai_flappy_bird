1ce570ee9b55a411e4e7c31573e8f60c1d73f9ad
Conqcd <411277487@qq.com>
2025-02-20 22:37:03 +0800

    change adamW

--- PPO.py
+++ PPO.py
@@ -62,7 +62,7 @@ def compute_returns(rewards, values, gamma, gae_lambda):
 def ppo_update(policy_net, value_net, optimizer, states, actions, log_probs, returns, advantages, clip_epsilon=0.2,max_grad_norm=1.0):
     wa = 1
     wv = 1
-    we = 0.5
+    we = 0.01
     for _ in range(10):  # Update for 10 epochs
         action_probs = policy_net(states)
         two_probs = torch.stack([action_probs[:,0], 1 - action_probs[:,0]], dim=1)
@@ -107,7 +107,7 @@ def main():
         policy_net = PolicyNetwork(action_dim).to(device)
         value_net = ValueNetwork(action_dim).to(device)
     params = list(policy_net.parameters()) + list(value_net.parameters())
-    optimizer = optim.Adam(params, lr=1e-4)
+    optimizer = optim.AdamW(params, lr=1e-4, amsgrad=True,weight_decay=0.001)
 
     max_episodes = 10000
     gamma = 0.99
@@ -124,10 +124,10 @@ def main():
                 action_probs = policy_net(state_tensor)
                 value = value_net(state_tensor).detach()
             two_probs = torch.stack([action_probs[:,0], 1 - action_probs[:,0]], dim=1)
-            print(two_probs.detach().cpu().numpy())
             dist = Categorical(two_probs)
             action = dist.sample()
             log_prob = dist.log_prob(action)
+            print(two_probs.detach().cpu().numpy(),action.detach().cpu().numpy())
 
             next_state, reward, done, _ = env.step(action.item())
 
